51f6cd673b4b2b892d1cf04938c2e1ea7956b171
Abhijit1.Mishra <abhijit1.mishra@ril.com>
2024-10-29 02:25:13 +0530

    updated ui

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,20 @@
+// 'use client'
+
+// import Counter from "./components/Counter";
+// import Fetching from "./components/Fetching";
+
+// export default function Home() {
+//   return (
+//     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+//       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+//         {/* <h1 className="text-5xl">Hello</h1> */}
+//       </main>
+//       <Counter className="w-9/12 h-9/12" />
+//       <Fetching />
+//     </div>
+//   );
+// }
+
 'use client'
 
 import Counter from "./components/Counter";
@@ -5,12 +22,8 @@ import Fetching from "./components/Fetching";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        {/* <h1 className="text-5xl">Hello</h1> */}
-      </main>
-      <Counter className="w-9/12 h-9/12" />
-      <Fetching />
-    </div>
+  <div className="flex flex-col flex-row items-center">
+    <Fetching />
+  </div>    
   );
 }
