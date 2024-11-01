bbc81d110a443d6df828127050f9254068d15779
Abhijit1.Mishra <abhijit1.mishra@ril.com>
2024-11-02 03:33:21 +0530

    font fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,12 +5,13 @@ import "./globals.css";
 const geistSans = localFont({
   src: "../public/fonts/GeistVF.woff",
   variable: "--font-geist-sans",
-  weight: "100 900",
+  weight: "100 200 300 400 500 600 700 800 900", // Include all weights
 });
+
 const geistMono = localFont({
   src: "../public/fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
-  weight: "100 900",
+  weight: "100 200 300 400 500 600 700 800 900", // Include all weights
 });
 
 export const metadata = {
@@ -27,6 +28,5 @@ export default function RootLayout({ children }) {
         {children}
       </body>
     </html>
-
   );
 }
